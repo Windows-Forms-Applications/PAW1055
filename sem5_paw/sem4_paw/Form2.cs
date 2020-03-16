eb5bc3e7a372b7745ad5f30113f805a074246464
Cristian Ciurea <cristianciurea@gmail.com>
2020-03-16 22:35:29 +0200

    update

---
+++ sem5_paw/sem4_paw/Form2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace sem4_paw
+{
+    public partial class Form2 : Form
+    {
+        List<Student> lista2;
+
+        public Form2(List<Student> lista)
+        {
+            InitializeComponent();
+            lista2 = lista;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            foreach (Student s in lista2)
+                textBox1.Text += s.ToString() + Environment.NewLine;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //SaveFileDialog dlg = new SaveFileDialog();
+            //dlg.Filter = "";
+            saveFileDialog1.Filter = "(*.txt)|*.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+                sw.WriteLine(textBox1.Text);
+                sw.Close();
+                textBox1.Clear();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "(*.txt)|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                textBox1.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = new FileStream("fisier.dat", FileMode.Create, FileAccess.Write);
+            //varianta 1
+            //bf.Serialize(fs, textBox1.Text);
+            //varianta 2
+            bf.Serialize(fs, lista2);
+            fs.Close();
+            textBox1.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = new FileStream("fisier.dat", FileMode.Open, FileAccess.Read);
+            //varianta 1
+            //textBox1.Text = (string)bf.Deserialize(fs);
+            //varianta 2
+            textBox1.Clear();
+            List<Student> lista3 = (List<Student>)bf.Deserialize(fs);
+            foreach (Student s in lista3)
+                textBox1.Text += s.ToString() + Environment.NewLine;
+            fs.Close();
+        }
+    }
+}
---
+++ sem5_paw/sem4_paw/Persoana.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sem4_paw
+{
+    [Serializable]
+    public abstract class Persoana
+    {
+        protected int cod;
+        protected char sex;
+
+        public Persoana()
+        {
+            cod = 0;
+            sex = 'F';
+        }
+
+        public Persoana(int c, char s)
+        {
+            cod = c;
+            sex = s;
+        }
+
+        public override string ToString()
+        {
+            return "Persoana cu codul " + cod + " are sexul " + sex;
+        }
+
+        public abstract void SpuneAnNastere();
+    }
+}
